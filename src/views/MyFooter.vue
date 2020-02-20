d579011c598da24cded5c40c30fc7995e1c97222
Youngkun <youngkunzhu@163.com>
2020-02-20 15:36:31 +0800

    加入备案号

--- src/views/MyFooter.vue
+++ src/views/MyFooter.vue
@@ -1,8 +1,19 @@
 <template>
-    <Footer>
-        <div style="text-align: center">
+    <Footer style="display: flex">
+        <div>
             @CopyRight youngkun.site
         </div>
+        <a target="_blank"
+           href="http://www.beian.miit.gov.cn/"
+           style="color: gray;text-decoration: none; white-space: nowrap; display: flex; align-items: center; margin-left: auto">
+            浙ICP备20001410号-1
+        </a>
+        <a target="_blank"
+           href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33078202001132"
+           style="color: gray; text-decoration:none; white-space: nowrap; display: flex; align-items: center; margin-left: 24px">
+            <img src="@/assets/beian.png"/>
+            浙公网安备33078202001132号
+        </a>
     </Footer>
 </template>
 
